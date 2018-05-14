2bfed3a10760731dbcc40a0444a9804c3642dede
rikyribeiro3000 <35766691+rikyribeiro3000@users.noreply.github.com>
2018-05-14 21:40:44 +0100

    redid pause and start menu

---
+++ Assets/scripts/Mainmenu/ShowPanels.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowPanels : MonoBehaviour {
+
+	public GameObject optionsPanel;							//Store a reference to the Game Object OptionsPanel 
+	public GameObject optionsTint;							//Store a reference to the Game Object OptionsTint 
+	public GameObject menuPanel;							//Store a reference to the Game Object MenuPanel 
+
+
+
+	//Call this function to activate and display the Options panel during the main menu
+	public void ShowOptionsPanel()
+	{
+		optionsPanel.SetActive(true);
+		optionsTint.SetActive(true);
+	}
+
+	//Call this function to deactivate and hide the Options panel during the main menu
+	public void HideOptionsPanel()
+	{
+		optionsPanel.SetActive(false);
+		optionsTint.SetActive(false);
+	}
+
+	//Call this function to activate and display the main menu panel during the main menu
+	public void ShowMenu()
+	{
+		menuPanel.SetActive (true);
+	}
+}
---
+++ Assets/scripts/PauseMenu.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+    public static bool GameIsPaused = false;
+    Resolution[] resolutions;
+    public GameObject vidas;
+    public GameObject perguntas;
+    public AudioMixer audioMixer;
+    public Slider volumeSlider;
+    public Dropdown resolutionDropdown;
+    public GameObject pauseMenuUI;
+    // Update is called once per frame
+    void Update () {
+        volumeSlider.value = AudioListener.volume;
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                vidas.SetActive(true);
+                perguntas.SetActive(true);
+                Resume();
+            } else
+            {
+                vidas.SetActive(false);
+                perguntas.SetActive(false);
+                Pause();
+            }
+        }
+		
+	}
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume( float volume)
+    {
+        AudioListener.volume = volumeSlider.value;
+    }
+    public void Resume ()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+
+    }
+
+    void Pause ()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Application.LoadLevel("Main");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
+
