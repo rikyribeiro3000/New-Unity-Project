4910261f3aa5be1baf2d42084bdb158a876363c4
rikyribeiro3000 <35766691+rikyribeiro3000@users.noreply.github.com>
2018-05-21 23:13:36 +0100

    added a binary save file

---
+++ Assets/scripts/save.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System;
+using System.IO;
+
+public static class Save
+{
+
+    public static void Savetry(gameover trys)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream stream = new FileStream(Application.persistentDataPath + "/player.tet", FileMode.Create);
+
+        PlayerData data = new PlayerData(trys);
+
+        bf.Serialize(stream, data);
+        stream.Close();
+    }
+
+     public static int[] LoadPlayer()
+     {
+         if (File.Exists(Application.persistentDataPath + "/player.tet"))
+         {
+             BinaryFormatter bf = new BinaryFormatter();
+             FileStream stream = new FileStream(Application.persistentDataPath + "/player.tet", FileMode.Open);
+
+             PlayerData data = bf.Deserialize(stream) as PlayerData;
+             stream.Close();
+             return data.trysa;
+         } else
+            {
+            Debug.LogError("something");
+            return new int[0];
+            }
+     }
+}
+[Serializable]
+public class PlayerData
+ {
+    public int[] trysa;
+
+     public PlayerData(gameover trys)
+    {
+        trysa = new int[1];
+        trysa[0] = trys.trys;
+    }
+ }
+ 
